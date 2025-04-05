d86bef63144843b16808eca989a0ab2bd525b444
Neha-Himesh <neha.himesh@spike.sh>
2025-04-05 17:40:55 +0530

    Added js code for form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,16 +14,51 @@ document.addEventListener("DOMContentLoaded", function () {
    
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
+            const className = entry.target.getAttribute("data-name");
             if (entry.isIntersecting) {
-                const className = entry.target.getAttribute("data-name");
+              
                 entry.target.classList.add(className); // Animate width when visible
                 console.log("Into the view");
+            } else{
+                if(entry.target.classList.contains(className)){
+                    console.log("Outside the view");
+                    entry.target.classList.remove(className);
+                }
             }
         });
     }, { threshold: 0.5 }); // Trigger when 50% visible
 
     progressBars.forEach((bar) => observer.observe(bar));
-});
+    const formButton = document.getElementById("form-submit");
+    var form = document.getElementById("portfolio-form");
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent default form submit behavior
+    
+        const formData = new FormData(form);
+    
+        fetch(form.action, {
+          method: 'POST',
+          body: formData,
+          headers: {
+            'Accept': 'application/json'
+          }
+        })
+        .then(response => {
+          if (response.ok) {
+            // ✅ Success: Clear form + show message
+            form.reset();
+            alert("Thank you! Your message has been sent.");
+          } else {
+            alert('Oops! Something went wrong with submission.');
+          }
+        })
+        .catch(error => {
+          console.error('Form submission error:', error);
+          alert('There was an error. Please try again later.');
+        });
+    });
+    })
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
